4fc23400cbbbe224063f64d797e9919b06739c1b
Abhishek Kumar <dk9554654249@gmail.com>
2022-09-22 00:17:19 +0530

    completed

---
+++ script.js
@@ -0,0 +1,13 @@
+let query="AbhishekKumar";
+const qrImg = document.getElementById('qr-image');
+let url='';
+url="https://api.qrserver.com/v1/create-qr-code/?size=160x160&data="+query;
+qrImg.style.background = `url(${url})`;
+function handlesubmit(){
+    console.log("Handle submit invoked");
+    query=document.getElementById("qr-text").value;
+    url="https://api.qrserver.com/v1/create-qr-code/?size=160x160&data="+query.replace(" ","+");
+    qrImg.style.background = `url(${url})`; 
+    console.log(query);
+}
+console.log(url);
